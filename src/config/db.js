8db4823e9ab70b33fd856cf85d5e4fba6e7b87f3
CodeHumani <sossamarty@gmail.com>
2024-06-15 05:36:38 -0400

    Update database connection configuration for Render

--- src/config/db.js
+++ src/config/db.js
@@ -2,11 +2,10 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'guitar',
-  password: 'Medrano73697178',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
